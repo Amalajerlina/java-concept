89fe93c55d4227e7ea0b9fc3cb9a6da80b699aef
Amalajerlina <jerlina331@gmail.com>
2025-10-21 15:27:16 +0530

    initial commit

--- Batch_C77/src/com/tnsif/oopsconcept/AbstractionDemo.java
+++ Batch_C77/src/com/tnsif/oopsconcept/AbstractionDemo.java
@@ -12,13 +12,17 @@ class Circle extends Shape{
 }
 
 
-public class AbstractionDemo {
+public class com.tnsif.oopsconcept;
+ {
 
-	public static void main(String[] args) {
-		
-		Shape s = new Circle();
-		s.draw();
-		
-	}
-
+	abstract class Shape{
+	abstract void draw(); 
 }
+
+class Circle extends Shape{
+
+	void draw() {
+		System.out.println("Drawing Circle");
+	}
+}
+
